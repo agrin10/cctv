53d48f0aa64c08a948c71420b5eb8f67352441e9
agrin10 <agrinflame2@gmail.com>
2024-10-14 17:39:03 +0330

    implemented persian numbers for UI

--- app.py
+++ app.py
@@ -6,6 +6,16 @@ from flask import request , url_for , render_template ,redirect
 app = create_app()
 
 
+def to_persian_digits(number):
+    persian_digits = '۰۱۲۳۴۵۶۷۸۹'
+    return ''.join(persian_digits[int(digit)] if digit.isdigit() else digit for digit in str(number))
+
+# Registering it as a filter in Jinja2
+app.jinja_env.filters['persian'] = to_persian_digits
 
 if __name__ == "__main__":
     app.run(debug=True , port=8080 , host='0.0.0.0')
+
+
+
+
