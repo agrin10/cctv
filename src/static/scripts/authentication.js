c638ec1ae13afce0760d5c178f2aefb29da3f824
agrin10 <agrinflame2@gmail.com>
2024-11-20 20:02:02 +0330

    added blocks to base

---
+++ src/static/scripts/authentication.js
@@ -0,0 +1,19 @@
+function openTab(event, tabName) {
+    let i, tabcontent, tablinks;
+
+    tabcontent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tab-link");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(tabName).style.display = "block";
+    event.currentTarget.className += " active";
+}
+
+// Set default active tab
+document.getElementById("login").style.display = "block";
