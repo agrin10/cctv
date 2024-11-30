d604b469490c3b2d85f7608ca44e9526ec2cf274
agrin10 <agrinflame2@gmail.com>
2024-11-30 21:43:16 +0330

    modified config file

--- config.py
+++ config.py
@@ -7,46 +7,41 @@ load_dotenv()
 class Config:
     DEBUG = False
     TESTING = False
-    SQLALCHEMY_TRACK_MODIFICATION = False
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    UPLOAD_FOLDER = os.getenv('UPLOAD_FOLDER')
-    UPLOAD_EXTENSIONS =os.getenv('UPLOAD_EXTENSIONS')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
+    UPLOAD_FOLDER = os.getenv('UPLOAD_FOLDER', 'uploads')
+    
+    UPLOAD_EXTENSIONS = os.getenv('UPLOAD_EXTENSIONS', '').split(',')
 
-
-    # coookies and toke configuration
-    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'default_jwt_secret')
     JWT_TOKEN_LOCATION = ['cookies']
     JWT_REFRESH_COOKIE_PATH = os.getenv('JWT_REFRESH_COOKIE_PATH')
     JWT_ACCESS_COOKIE_NAME = os.getenv('JWT_ACCESS_COOKIE_NAME')
     JWT_REFRESH_COOKIE_NAME = os.getenv('JWT_REFRESH_COOKIE_NAME')
-    JWT_COOKIE_SECURE = False 
+    JWT_COOKIE_SECURE = False
     JWT_COOKIE_CSRF_PROTECT = False
     JWT_CSRF_IN_COOKIES = False
-    PERMANENT_SESSION_LIFETIME = timedelta(days=1) 
+    PERMANENT_SESSION_LIFETIME = timedelta(days=1)
     REMEMBER_COOKIE_DURATION = timedelta(days=7)
-    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=6)  
-    JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=30)  
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=6)
+    JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=30)
 
-
-
-    # api urls
-    AI_URL = os.getenv('AI_URL')
-    RECORDING_URL = os.getenv('RECORDING_URL')
-
+    # API URLs
+    AI_URL = os.getenv('AI_URL', 'http://localhost/')
+    RECORDING_URL = os.getenv('RECORDING_URL', 'http://localhost/')
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
-    RTSP_URL = os.getenv('RTSP_URL')
-
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI', 'sqlite:///development.db')
+    RTSP_URL = os.getenv('RTSP_URL', '')
 
 class TestingConfig(Config):
     TESTING = True
-
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI', 'sqlite:///testing.db')
 
 class ProductionConfig(Config):
     DEBUG = False
-    SESSION_COOKIE_SECURE=True
+    SESSION_COOKIE_SECURE = True
     JWT_COOKIE_CSRF_PROTECT = True
-    JWT_COOKIE_SECURE = False 
-
+    JWT_COOKIE_SECURE = True
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI', 'sqlite:///production.db')
